e5ac41ac00bd03788b83802147f0d33a5675efb2
Arlind-Pecmarkaj <95414401+Arlind-Pecmarkaj@users.noreply.github.com>
2021-12-02 15:56:22 +0100

    Aggiunta enum alimentazione
    Aggiunto il primo enum

---
+++ Parcheggio/src/parcheggio/Enum/Alimentazione.java
@@ -0,0 +1,12 @@
+package parcheggio.Enum;
+
+/**
+ * @author Arlind Pecmarkaj
+ */
+public enum Alimentazione {
+    BENZINA,
+    DIESEL,
+    GPL,
+    METANO,
+    ELETTRICA;
+}
---
+++ Parcheggio/src/parcheggio/ParcheggioMain.java
@@ -0,0 +1,17 @@
+
+package parcheggio;
+
+/**
+ *
+ * @author arlin
+ */
+public class ParcheggioMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
