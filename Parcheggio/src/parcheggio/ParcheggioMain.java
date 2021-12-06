4df16ea635f7028ae38d1c8e9baee219352acaa4
Arlind-Pecmarkaj <95414401+Arlind-Pecmarkaj@users.noreply.github.com>
2021-12-06 14:52:30 +0100

    (Prova)
    Da ignorare: commit di prova per verifica funzionamento

--- Parcheggio/src/parcheggio/ParcheggioMain.java
+++ Parcheggio/src/parcheggio/ParcheggioMain.java
@@ -11,7 +11,7 @@ public class ParcheggioMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    	System.out.println("Prova");
     }
     
 }
