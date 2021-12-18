fa959abf981d1c617e5812985fc0f7462269e7fd
Arlind-Pecmarkaj <95414401+Arlind-Pecmarkaj@users.noreply.github.com>
2021-12-18 18:14:36 +0100

    Create Test.java
    Aggiunta classe per i test.

---
+++ Parcheggio/src/parcheggio/Test/Test.java
@@ -0,0 +1,14 @@
+package parcheggio.Test;
+
+/* Classe riservata ai test delle classi e dei metodi.
+ * Specificare nominativo, piccolo commento sul test da effettuare
+ * ed eventuali problemi se fossero presenti.
+ * NOTA: separare ogni fase di test. */
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+}
