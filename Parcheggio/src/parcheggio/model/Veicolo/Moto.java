081d37b9cd665b81bfb9575b13b310e5eac1b8e3
Martin Berardi <76852603+martin-berardi@users.noreply.github.com>
2021-12-29 17:02:49 +0100

    Aggiunta classe Moto

---
+++ Parcheggio/src/parcheggio/model/Veicolo/Moto.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progettopmo;
+
+/**
+ *
+ * @author marti
+ */
+public class Moto extends Veicolo{
+    private double velocita;
+    
+    public Moto(String targa, int annoImm, Alimentazione carburante,
+                    String marca, String modello, String nomeProprietario,
+                    String cognomeProprietario, double v){
+        super(targa, annoImm, carburante, marca, modello, nomeProprietario, cognomeProprietario);
+        this.velocita = v;
+    }
+    
+    public double getVelocita(){
+        return this.velocita;
+    }
+    
+    public void setVelocita(double v){
+        this.velocita = v;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        return super.toString() + "\nVelocita': " + this.velocita;
+    }
+}
