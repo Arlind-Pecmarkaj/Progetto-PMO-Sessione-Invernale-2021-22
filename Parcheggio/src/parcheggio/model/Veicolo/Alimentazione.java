2057522cd68aef076d35e09b6912484c0c51d18e
Martin Berardi <76852603+martin-berardi@users.noreply.github.com>
2021-12-29 17:04:47 +0100

    Aggiunta Alimentazione

---
+++ Parcheggio/src/parcheggio/model/Veicolo/Alimentazione.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progettopmo;
+
+/**
+ *
+ * @author marti
+ */
+public enum Alimentazione {
+    BENZINA,
+    DIESEL,
+    GPL,
+    METANO,
+    ELETTRICA;
+}
+
