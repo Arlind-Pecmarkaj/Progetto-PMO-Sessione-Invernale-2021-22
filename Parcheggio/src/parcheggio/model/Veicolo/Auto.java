3aee2d0da738f4d44ac47e5b41665d9de63bdb6b
Martin Berardi <76852603+martin-berardi@users.noreply.github.com>
2021-12-29 17:02:25 +0100

    Aggiunta Auto.java

---
+++ Parcheggio/src/parcheggio/model/Veicolo/Auto.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progettopmo;
+
+/**
+ *
+ * @author marti
+ */
+public class Auto extends Veicolo{
+    private double altezza;
+    private int nPosti;
+    
+    public Auto(String targa, int annoImm, Alimentazione carburante,
+                    String marca, String modello, String nomeProprietario,
+                    String cognomeProprietario, double altezza, int nPosti){
+        super(targa, annoImm, carburante, marca, modello, nomeProprietario, cognomeProprietario);
+        this.altezza = altezza;
+        this.nPosti = nPosti;
+    }
+    
+    public double getAltezza(){
+        return this.altezza;
+    }
+    
+    public int getNPosti(){
+        return this.nPosti;
+    }
+    
+    public void setAltezza(double alt){
+        this.altezza = alt;
+    }
+    
+    public void setNPosti(int np){
+        this.nPosti = np;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        return super.toString() + "\nAltezza: " + this.altezza
+                + "\nN° posti: " + this.nPosti;
+    }
+}
