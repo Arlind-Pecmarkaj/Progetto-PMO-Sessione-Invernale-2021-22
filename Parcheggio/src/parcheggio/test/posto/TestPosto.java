a53b45054b11ef9629b99c886db7b975f1fe8434
petrello <tommasop01@gmail.com>
2022-01-01 19:07:39 +0100

    Update TestPosto.java
    Aggiunti nuovi test

--- Parcheggio/src/parcheggio/test/posto/TestPosto.java
+++ Parcheggio/src/parcheggio/test/posto/TestPosto.java
@@ -12,25 +12,53 @@ package parcheggio.test.posto;
 //import static org.junit.jupiter.api.Assertions.*;
 //import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
+
+import org.graalvm.compiler.lir.phases.PostAllocationOptimizationStage_OptionDescriptors;
 import org.junit.*;
+
+import parcheggio.model.posto.*;
+import parcheggio.enumerations.Alimentazione;
+import parcheggio.model.posto.*;
+import parcheggio.model.sensore.Sensore;
+
 import java.util.*;
 
 public class TestPosto {
+	
+	APosto postoMoto;
 
 	@Before
 	public void createData() {
 		// inizializzazione prima di eseguire un qualunque test
 		
+		
+		
+//		IPosto postoAuto = new PostoAuto(new SensoreCarburante());
+//		postoAuto.setPosto("1", 1.0);
+		postoMoto = new PostoMoto();
+		postoMoto.setPosto("1", 1.0);
+//		IPosto postoMoto = new PostoMoto();
+//		postoMoto.setPosto();
+		
+	}
+	
+	@Test
+	public void testTempleteMethodSetPosto() {
+		// controllo se viene settato il gisuto ID
+		assertEquals("M1", postoMoto.getId()); 	
+		// controllo se viene settata la giusta tariffa oraria
+		assertEquals(0.5, postoMoto.getId());
 	}
 	
 	@Test
 	public void testIsLibero() {
-		
+		// controllo se un parcheggio è libero
+		assertTrue(postoMoto.isLibero());
 	}
 	
 	@Test
 	public void testTempoOccupazione() {
-		
+		// TODO
 	}
 	
 //	@Test(expected = IllegalStateException.class)
