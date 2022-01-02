d962f5761e89ee1cc75de7ce63397569970dc37d
MARTIN <martin.berardi15@gmail.com>
2022-01-02 17:26:20 +0100

    Update Moto.java

--- Parcheggio/src/parcheggio/model/veicolo/Moto.java
+++ Parcheggio/src/parcheggio/model/veicolo/Moto.java
@@ -9,29 +9,23 @@ package parcheggio.model.veicolo;
  * @author marti
  */
 public class Moto extends Veicolo{
-    private double velocita;
     
     public Moto(String targa, int annoImm, Alimentazione carburante,
                     String marca, String modello, String nomeProprietario,
                     String cognomeProprietario, double v){
-        super(targa, annoImm, carburante, marca, modello, nomeProprietario, cognomeProprietario);
-        this.velocita = v;
+        
+    	super(targa, annoImm, carburante, marca, modello, nomeProprietario, cognomeProprietario);
     }
     
-    public double getVelocita(){
-        return this.velocita;
+    public boolean equals(Moto m){
+    	return this.getCarburante() == m.getCarburante();
     }
-    
-    public void setVelocita(double v){
-        this.velocita = v;
-    }
-    
     /**
      *
      * @return
      */
     @Override
     public String toString(){
-        return super.toString() + "\nVelocita': " + this.velocita;
+        return super.toString() + "\nAlimentazione: " + this.getCarburante();
     }
 }
