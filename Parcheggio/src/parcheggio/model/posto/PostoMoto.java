99e06b018be0d795f82c83ef5a642c16b771c9ce
MARTIN <martin.berardi15@gmail.com>
2022-01-02 13:57:57 +0100

    Create PostoMoto.java

---
+++ Parcheggio/src/parcheggio/model/posto/PostoMoto.java
@@ -0,0 +1,31 @@
+package parcheggio.model.posto;
+
+import static parcheggio.enumerations.TassaParcheggio.*;
+
+public class PostoMoto extends APosto {
+	
+	/**
+	 * 	Compone una parte variabile univoca con il codice "M"
+	 * 	che sta ad indicare che il posto è riservato alle Moto.
+	 * 	L'ID di un posto corrisponde con il numero suo numero.
+	 * 	
+	 * 	@param postoMotoID
+	 * 	@return l'ID composto di un posto per moto
+	 */
+	@Override
+	protected String setId(String postoMotoID) {
+		return "M" + postoMotoID;
+	}
+
+	/**
+	 * 	Calcola la tariffa oraria di questo posto in un certo parcheggio.
+	 * 
+	 * 	@param standardTax
+	 * 	@return tariffa oraria per moto
+	 */
+	@Override
+	protected double setCostoOrario(double standardTax) {
+		return standardTax / TASSA_MOTO.getTaxValue();
+	}
+
+}
