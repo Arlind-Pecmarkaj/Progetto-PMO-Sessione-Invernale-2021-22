2d626896c0da80193ea5a1a24709441dce94866d
LeonardoBigelli <l.bigelli2@campus.uniurb.it>
2022-01-04 16:48:57 +0100

    Aggiunto metodo per aggiungere abbonamenti

--- Parcheggio/src/parcheggio/model/Parcheggio.java
+++ Parcheggio/src/parcheggio/model/Parcheggio.java
@@ -146,6 +146,10 @@ public class Parcheggio {
 		return prezzo;
 	}
 
+	public void aggiungiAbbonamento(Abbonamento a) {
+		this.abbonamenti.add(a);
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
