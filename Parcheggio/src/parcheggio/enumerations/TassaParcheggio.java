c4f48304e4938bf6b1d240c4963bc66a6cb899da
petrello <tommasop01@gmail.com>
2022-01-05 18:02:04 +0100

    Update TassaParcheggio.java
    rimossi dai commenti i caratteri speciali non leggibili

--- Parcheggio/src/parcheggio/enumerations/TassaParcheggio.java
+++ Parcheggio/src/parcheggio/enumerations/TassaParcheggio.java
@@ -1,10 +1,10 @@
 package parcheggio.enumerations;
 
 public enum TassaParcheggio {
-							// se la tassa fissa del parcheggio X è 1€...
-	TASSA_AUTO(0.8),		// => 1,25€ all'ora			
-	TASSA_MOTO(2.0),		// => 0,50€ all'ora
-	TASSA_ELETTRICO(0.4);	// => 2,50€ all'ora
+							// se la tassa fissa del parcheggio X è 1 euro...
+	TASSA_AUTO(0.8),		// => 1,25 euro all'ora			
+	TASSA_MOTO(2.0),		// => 0,50 euro all'ora
+	TASSA_ELETTRICO(0.4);	// => 2,50 euro all'ora
 	
 	
 	private final double taxValue;
