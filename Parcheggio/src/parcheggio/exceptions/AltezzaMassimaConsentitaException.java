849ec8e47824815ca78b6cbd802e67609ae48d5c
MARTIN <martin.berardi15@gmail.com>
2022-01-06 15:07:09 +0100

    Create AltezzaMassimaConsentitaException.java

---
+++ Parcheggio/src/parcheggio/exceptions/AltezzaMassimaConsentitaException.java
@@ -0,0 +1,7 @@
+package parcheggio.exceptions;
+
+public class AltezzaMassimaConsentitaException extends RuntimeException{
+	public AltezzaMassimaConsentitaException(String msg) {
+		super(msg);
+	}
+}
