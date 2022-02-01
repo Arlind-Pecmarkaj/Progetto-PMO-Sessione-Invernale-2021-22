487760d157a65cadcea1531d0ab0892ed5d58375
Arlind-Pecmarkaj <95414401+Arlind-Pecmarkaj@users.noreply.github.com>
2022-02-01 18:23:58 +0100

    Migliorie grafiche e di codice a GUIGestione

--- Parcheggio/src/parcheggio/view/GUI/GUIGestione.java
+++ Parcheggio/src/parcheggio/view/GUI/GUIGestione.java
@@ -3,6 +3,7 @@ package parcheggio.view.GUI;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.AttributeSet.ColorAttribute;
 
 import parcheggio.controller.ReaderWriterFromFile;
 import parcheggio.model.GestioneParcheggio;
@@ -21,6 +22,7 @@ import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JSeparator;
@@ -33,12 +35,15 @@ import static javax.swing.JOptionPane.showMessageDialog;
 public class GUIGestione extends JFrame {
 
 	private JPanel       contentPane;
+	private JPanel panel_abbonamenti;
+	private JPanel   panel_parcheggi;
 	private JTextField codiceFiscale;
 	private JTextField          nome;
 	private JTextField       cognome;
 	private JTextField   dataNascita;
 	private JTextField   nazionalita;
 	private JTextField         targa;
+
 
 	/**
 	 * Costruisco il frame
@@ -49,33 +54,38 @@ public class GUIGestione extends JFrame {
 		setBounds(100, 100, 810, 470); /* la finestra sara' di 810x470 px con il vertice superiore sinistro nella posizione (100,100). */
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5)); /* margini della finestra. */
+		contentPane.setBackground(Color.gray.brighter());
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWidths  = new int[]{0, 0};
+		gbl_contentPane.rowHeights    = new int[]{0, 0, 0, 0, 0, 0, 0};
 		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights    = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
-		JLabel lblNewLabel = new JLabel("Seleziona parcheggio");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 0;
-		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		JLabel lblSelezParcheggio                 = new JLabel("Seleziona parcheggio");
+		GridBagConstraints gbcLblSelezParcheggio = new GridBagConstraints();
+		gbcLblSelezParcheggio.insets             = new Insets(0, 0, 5, 0);
+		gbcLblSelezParcheggio.gridx              = 0;
+		gbcLblSelezParcheggio.gridy = 0;
+		contentPane.add(lblSelezParcheggio, gbcLblSelezParcheggio);
 		
-		JPanel panel_parcheggi = new JPanel(); /* Panel per la visualizzazione dei parcheggi */
+		panel_parcheggi = new JPanel();
+		panel_parcheggi.setOpaque(true);
+		panel_parcheggi.setBackground(Color.gray);
 		GridBagConstraints gbc_panel_parcheggi = new GridBagConstraints();
-		gbc_panel_parcheggi.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_parcheggi.fill = GridBagConstraints.BOTH;
-		gbc_panel_parcheggi.gridx = 0;
-		gbc_panel_parcheggi.gridy = 1;
+		gbc_panel_parcheggi.insets             = new Insets(0, 0, 5, 0);
+		gbc_panel_parcheggi.fill               = GridBagConstraints.BOTH;
+		gbc_panel_parcheggi.gridx              = 0;
+		gbc_panel_parcheggi.gridy              = 1;
 		contentPane.add(panel_parcheggi, gbc_panel_parcheggi);
 		panel_parcheggi.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5)); //Per i parcheggi si usa un flow layout.
 		
 		g.aggiornaAbbonamenti();
 		for (ParcheggioImpl p : g.getParcheggi()) {
 			JButton bottone_parcheggio = new JButton(p.getName());
+			bottone_parcheggio.setBackground(Color.gray.darker());
+			bottone_parcheggio.setForeground(Color.white);
 			bottone_parcheggio.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					new GUIParcheggio(p);
@@ -86,161 +96,173 @@ public class GUIGestione extends JFrame {
 		
 		JSeparator separator = new JSeparator();
 		GridBagConstraints gbc_separator = new GridBagConstraints();
-		gbc_separator.insets = new Insets(0, 0, 5, 0);
-		gbc_separator.gridx = 0;
-		gbc_separator.gridy = 2;
+		gbc_separator.insets             = new Insets(0, 0, 5, 0);
+		gbc_separator.gridx              = 0;
+		gbc_separator.gridy              = 2;
 		contentPane.add(separator, gbc_separator);
 		
-		JLabel lblNewLabel_1 = new JLabel("Inserimento abbonamento");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 4;
-		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		JLabel lblInserimentoAbbonamento = new JLabel("Inserimento abbonamento");
+		GridBagConstraints gbcLblInsAbbonamento = new GridBagConstraints();
+		gbcLblInsAbbonamento.insets             = new Insets(0, 0, 5, 0);
+		gbcLblInsAbbonamento.gridx              = 0;
+		gbcLblInsAbbonamento.gridy              = 4;
+		contentPane.add(lblInserimentoAbbonamento, gbcLblInsAbbonamento);
 		
-		JPanel panel_abbonamenti = new JPanel(); /* Panel per gli abbonamenti */
+		panel_abbonamenti = new JPanel();
+		panel_abbonamenti.setBackground(Color.gray);
 		GridBagConstraints gbc_panel_abbonamenti = new GridBagConstraints();
-		gbc_panel_abbonamenti.fill = GridBagConstraints.BOTH;
-		gbc_panel_abbonamenti.gridx = 0;
-		gbc_panel_abbonamenti.gridy = 5;
+		gbc_panel_abbonamenti.fill               = GridBagConstraints.BOTH;
+		gbc_panel_abbonamenti.gridx              = 0;
+		gbc_panel_abbonamenti.gridy              = 5;
 		contentPane.add(panel_abbonamenti, gbc_panel_abbonamenti);
 		GridBagLayout gbl_panel_abbonamenti = new GridBagLayout();
-		gbl_panel_abbonamenti.columnWidths = new int[]{0, 0, 0};
-		gbl_panel_abbonamenti.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_abbonamenti.columnWidths  = new int[]{0, 0, 0};
+		gbl_panel_abbonamenti.rowHeights    = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel_abbonamenti.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_abbonamenti.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_abbonamenti.rowWeights    = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		panel_abbonamenti.setLayout(gbl_panel_abbonamenti);
 		
 		JLabel label_cf = new JLabel("Codice fiscale");
+		label_cf.setForeground(Color.white);
 		GridBagConstraints gbc_label_cf = new GridBagConstraints();
-		gbc_label_cf.anchor = GridBagConstraints.EAST;
-		gbc_label_cf.insets = new Insets(0, 0, 5, 5);
-		gbc_label_cf.gridx = 0;
-		gbc_label_cf.gridy = 0;
+		gbc_label_cf.anchor             = GridBagConstraints.EAST;
+		gbc_label_cf.insets             = new Insets(0, 0, 5, 5);
+		gbc_label_cf.gridx              = 0;
+		gbc_label_cf.gridy              = 0;
 		panel_abbonamenti.add(label_cf, gbc_label_cf);
 		
 		codiceFiscale = new JTextField();
 		GridBagConstraints gbc_codiceFiscale = new GridBagConstraints();
-		gbc_codiceFiscale.fill = GridBagConstraints.HORIZONTAL;
-		gbc_codiceFiscale.insets = new Insets(0, 0, 5, 0);
-		gbc_codiceFiscale.gridx = 1;
-		gbc_codiceFiscale.gridy = 0;
+		gbc_codiceFiscale.fill               = GridBagConstraints.HORIZONTAL;
+		gbc_codiceFiscale.insets             = new Insets(0, 0, 5, 0);
+		gbc_codiceFiscale.gridx              = 1;
+		gbc_codiceFiscale.gridy              = 0;
 		panel_abbonamenti.add(codiceFiscale, gbc_codiceFiscale);
 		codiceFiscale.setColumns(10);
 		
 		JLabel label_nome = new JLabel("Nome");
+		label_nome.setForeground(Color.white);
 		GridBagConstraints gbc_label_nome = new GridBagConstraints();
-		gbc_label_nome.anchor = GridBagConstraints.EAST;
-		gbc_label_nome.insets = new Insets(0, 0, 5, 5);
-		gbc_label_nome.gridx = 0;
-		gbc_label_nome.gridy = 1;
+		gbc_label_nome.anchor             = GridBagConstraints.EAST;
+		gbc_label_nome.insets             = new Insets(0, 0, 5, 5);
+		gbc_label_nome.gridx              = 0;
+		gbc_label_nome.gridy              = 1;
 		panel_abbonamenti.add(label_nome, gbc_label_nome);
 		
 		nome = new JTextField();
 		GridBagConstraints gbc_nome = new GridBagConstraints();
-		gbc_nome.fill = GridBagConstraints.HORIZONTAL;
-		gbc_nome.anchor = GridBagConstraints.NORTH;
-		gbc_nome.insets = new Insets(0, 0, 5, 0);
-		gbc_nome.gridx = 1;
-		gbc_nome.gridy = 1;
+		gbc_nome.fill               = GridBagConstraints.HORIZONTAL;
+		gbc_nome.anchor             = GridBagConstraints.NORTH;
+		gbc_nome.insets             = new Insets(0, 0, 5, 0);
+		gbc_nome.gridx              = 1;
+		gbc_nome.gridy              = 1;
 		panel_abbonamenti.add(nome, gbc_nome);
 		nome.setColumns(10);
 		
 		JLabel label_cognome = new JLabel("Cognome");
+		label_cognome.setForeground(Color.white);
 		GridBagConstraints gbc_label_cognome = new GridBagConstraints();
-		gbc_label_cognome.anchor = GridBagConstraints.EAST;
-		gbc_label_cognome.insets = new Insets(0, 0, 5, 5);
-		gbc_label_cognome.gridx = 0;
-		gbc_label_cognome.gridy = 2;
+		gbc_label_cognome.anchor             = GridBagConstraints.EAST;
+		gbc_label_cognome.insets             = new Insets(0, 0, 5, 5);
+		gbc_label_cognome.gridx              = 0;
+		gbc_label_cognome.gridy              = 2;
 		panel_abbonamenti.add(label_cognome, gbc_label_cognome);
 		
 		cognome = new JTextField();
 		GridBagConstraints gbc_cognome = new GridBagConstraints();
-		gbc_cognome.fill = GridBagConstraints.HORIZONTAL;
-		gbc_cognome.insets = new Insets(0, 0, 5, 0);
-		gbc_cognome.gridx = 1;
-		gbc_cognome.gridy = 2;
+		gbc_cognome.fill               = GridBagConstraints.HORIZONTAL;
+		gbc_cognome.insets             = new Insets(0, 0, 5, 0);
+		gbc_cognome.gridx              = 1;
+		gbc_cognome.gridy              = 2;
 		panel_abbonamenti.add(cognome, gbc_cognome);
 		cognome.setColumns(10);
 		
 		JLabel label_nascita = new JLabel("Data di nascita");
+		label_nascita.setForeground(Color.white);
 		GridBagConstraints gbc_label_nascita = new GridBagConstraints();
-		gbc_label_nascita.anchor = GridBagConstraints.NORTHEAST;
-		gbc_label_nascita.insets = new Insets(0, 0, 5, 5);
-		gbc_label_nascita.gridx = 0;
-		gbc_label_nascita.gridy = 3;
+		gbc_label_nascita.anchor             = GridBagConstraints.NORTHEAST;
+		gbc_label_nascita.insets             = new Insets(0, 0, 5, 5);
+		gbc_label_nascita.gridx              = 0;
+		gbc_label_nascita.gridy              = 3;
 		panel_abbonamenti.add(label_nascita, gbc_label_nascita);
 		
 		dataNascita = new JTextField();
 		GridBagConstraints gbc_dataNascita = new GridBagConstraints();
-		gbc_dataNascita.fill = GridBagConstraints.HORIZONTAL;
-		gbc_dataNascita.insets = new Insets(0, 0, 5, 0);
-		gbc_dataNascita.gridx = 1;
-		gbc_dataNascita.gridy = 3;
+		gbc_dataNascita.fill               = GridBagConstraints.HORIZONTAL;
+		gbc_dataNascita.insets             = new Insets(0, 0, 5, 0);
+		gbc_dataNascita.gridx              = 1;
+		gbc_dataNascita.gridy              = 3;
 		panel_abbonamenti.add(dataNascita, gbc_dataNascita);
 		dataNascita.setColumns(10);
 		
 		JLabel label_nazionalita = new JLabel("Nazionalita'");
+		label_nazionalita.setForeground(Color.white);
 		GridBagConstraints gbc_label_nazionalita = new GridBagConstraints();
-		gbc_label_nazionalita.anchor = GridBagConstraints.EAST;
-		gbc_label_nazionalita.insets = new Insets(0, 0, 5, 5);
-		gbc_label_nazionalita.gridx = 0;
-		gbc_label_nazionalita.gridy = 4;
+		gbc_label_nazionalita.anchor             = GridBagConstraints.EAST;
+		gbc_label_nazionalita.insets             = new Insets(0, 0, 5, 5);
+		gbc_label_nazionalita.gridx              = 0;
+		gbc_label_nazionalita.gridy              = 4;
 		panel_abbonamenti.add(label_nazionalita, gbc_label_nazionalita);
 		
 		nazionalita = new JTextField();
 		GridBagConstraints gbc_nazionalita = new GridBagConstraints();
-		gbc_nazionalita.insets = new Insets(0, 0, 5, 0);
-		gbc_nazionalita.fill = GridBagConstraints.HORIZONTAL;
-		gbc_nazionalita.gridx = 1;
-		gbc_nazionalita.gridy = 4;
+		gbc_nazionalita.insets             = new Insets(0, 0, 5, 0);
+		gbc_nazionalita.fill               = GridBagConstraints.HORIZONTAL;
+		gbc_nazionalita.gridx              = 1;
+		gbc_nazionalita.gridy              = 4;
 		panel_abbonamenti.add(nazionalita, gbc_nazionalita);
 		nazionalita.setColumns(10);
 		
 		JLabel label_targa = new JLabel("Targa");
+		label_targa.setForeground(Color.white);
 		GridBagConstraints gbc_label_targa = new GridBagConstraints();
-		gbc_label_targa.anchor = GridBagConstraints.EAST;
-		gbc_label_targa.insets = new Insets(0, 0, 5, 5);
-		gbc_label_targa.gridx = 0;
-		gbc_label_targa.gridy = 5;
+		gbc_label_targa.anchor             = GridBagConstraints.EAST;
+		gbc_label_targa.insets             = new Insets(0, 0, 5, 5);
+		gbc_label_targa.gridx              = 0;
+		gbc_label_targa.gridy              = 5;
 		panel_abbonamenti.add(label_targa, gbc_label_targa);
 		
 		targa = new JTextField();
 		GridBagConstraints gbc_targa = new GridBagConstraints();
-		gbc_targa.fill = GridBagConstraints.HORIZONTAL;
-		gbc_targa.insets = new Insets(0, 0, 5, 0);
-		gbc_targa.gridx = 1;
-		gbc_targa.gridy = 5;
+		gbc_targa.fill               = GridBagConstraints.HORIZONTAL;
+		gbc_targa.insets             = new Insets(0, 0, 5, 0);
+		gbc_targa.gridx              = 1;
+		gbc_targa.gridy              = 5;
 		panel_abbonamenti.add(targa, gbc_targa);
 		targa.setColumns(10);
 		
 		JLabel label_durata = new JLabel("Durata");
+		label_durata.setForeground(Color.white);
 		GridBagConstraints gbc_label_durata = new GridBagConstraints();
-		gbc_label_durata.anchor = GridBagConstraints.EAST;
-		gbc_label_durata.insets = new Insets(0, 0, 5, 5);
-		gbc_label_durata.gridx = 0;
-		gbc_label_durata.gridy = 6;
+		gbc_label_durata.anchor             = GridBagConstraints.EAST;
+		gbc_label_durata.insets             = new Insets(0, 0, 5, 5);
+		gbc_label_durata.gridx              = 0;
+		gbc_label_durata.gridy              = 6;
 		panel_abbonamenti.add(label_durata, gbc_label_durata);
 
 		
 		String[] opzioni = {"mensile", "trimestrale", "semestrale", "annuale"};
 		JComboBox<String> durata = new JComboBox<>(opzioni);
 		GridBagConstraints gbc_durata = new GridBagConstraints();
-		gbc_durata.insets = new Insets(0, 0, 5, 0);
-		gbc_durata.fill = GridBagConstraints.HORIZONTAL;
-		gbc_durata.gridx = 1;
-		gbc_durata.gridy = 6;
+		gbc_durata.insets             = new Insets(0, 0, 5, 0);
+		gbc_durata.fill               = GridBagConstraints.HORIZONTAL;
+		gbc_durata.gridx              = 1;
+		gbc_durata.gridy              = 6;
 		panel_abbonamenti.add(durata, gbc_durata);
 		
 		JCheckBox premium = new JCheckBox("Premium");
+		premium.setBackground(Color.gray);
+		premium.setForeground(Color.white);
 		GridBagConstraints gbc_premium = new GridBagConstraints();
-		gbc_premium.fill = GridBagConstraints.HORIZONTAL;
-		gbc_premium.insets = new Insets(0, 0, 5, 0);
-		gbc_premium.gridx = 1;
-		gbc_premium.gridy = 7;
+		gbc_premium.fill               = GridBagConstraints.HORIZONTAL;
+		gbc_premium.insets             = new Insets(0, 0, 5, 0);
+		gbc_premium.gridx              = 1;
+		gbc_premium.gridy              = 7;
 		panel_abbonamenti.add(premium, gbc_premium);
 		
 		JButton inserimento = new JButton("Inserisci abbonamento");
+		inserimento.setBackground(Color.gray.darker());
+		inserimento.setForeground(Color.white);
 		/* Cliccando su inserisci abbonamento viene inserito l'abbonamento in Gestione e viene aggiornata la lista di tutti gli abbonamenti. */
 		inserimento.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -277,8 +299,8 @@ public class GUIGestione extends JFrame {
 		});
 		
 		GridBagConstraints gbc_inserimento = new GridBagConstraints();
-		gbc_inserimento.gridx = 1;
-		gbc_inserimento.gridy = 8;
+		gbc_inserimento.gridx              = 2;
+		gbc_inserimento.gridy              = 7;
 		panel_abbonamenti.add(inserimento, gbc_inserimento);
 		
 		/* Alla chiusura del frame dovrò scrivere i dati */
