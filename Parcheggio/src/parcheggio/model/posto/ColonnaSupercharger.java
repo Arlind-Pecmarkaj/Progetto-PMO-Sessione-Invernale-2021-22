add14ce37df6094424c4eb752c6b3d2fc59f82cd
petrello <tommasop01@gmail.com>
2022-02-04 18:55:41 +0100

    Update ColonnaSupercharger.java
    Migliorato algoritmo di ricarica del veicolo e pulizia del codice per miglior funzionamento

--- Parcheggio/src/parcheggio/model/posto/ColonnaSupercharger.java
+++ Parcheggio/src/parcheggio/model/posto/ColonnaSupercharger.java
@@ -1,4 +1,6 @@
 package parcheggio.model.posto;
+
+import parcheggio.exceptions.IllegalChargerException;
 import parcheggio.model.veicolo.*;
 
 public class ColonnaSupercharger implements Supercharger {
@@ -7,10 +9,10 @@ public class ColonnaSupercharger implements Supercharger {
 	
 	public ColonnaSupercharger() {
 		this.kWh = 75.0;
-		this.tempoRicarica = 0.0;
+		this.tempoRicarica = 0;
 	}
 	
-	public void setTempoRicarica(double tempoRicarica) {
+	private void setTempoRicarica(double tempoRicarica) {
 		this.tempoRicarica = tempoRicarica;
 	}
 
@@ -24,14 +26,31 @@ public class ColonnaSupercharger implements Supercharger {
 		return this.tempoRicarica;
 	}
 	
+	/**
+	 *  Metodo per ottenere il tempo di ricarica del veicolo
+	 *  con un formato "human readable" (HR)
+	 * 
+	 */
+	public String getTempoRicaricaHR() {
+		double min = this.tempoRicarica * 60;
+		return String.format("%02d min", (int) Math.ceil(min));
+	}
+	
+	public double getPercentualeAttuale(Veicolo veicoloElettrico) {
+		double percentualeAttuale = (100 * veicoloElettrico.getCarburanteAttuale()) / veicoloElettrico.getCapienzaMassima(); // 100kW rimasti => X% => max => 100%
+		return percentualeAttuale;
+	}
+	
 	@Override
 	public void ricaricaVeicolo(int percentualeRaggiungere, Veicolo veicoloElettrico) {
-		double percentualeAttuale = (100 * veicoloElettrico.getCarburanteAttuale()) / veicoloElettrico.getCapienzaMassima(); // 100kW rimasti => X% => max => 100%
-		double percentualeRicaricare = percentualeRaggiungere - percentualeAttuale; // 70% - 10% => ricarico 60%
-		double kWRicaricare = (veicoloElettrico.getCapienzaMassima() * percentualeRicaricare) / 100;
+		if(percentualeRaggiungere <= getPercentualeAttuale(veicoloElettrico)) throw new IllegalChargerException("Attenzione! La batteria ha già raggiunto questo livello.");
+		if(percentualeRaggiungere > 100) throw new IllegalChargerException("Attenzione! La batteria raggiunge al massimo il 100%");
 		
+		double percentualeRicaricare = percentualeRaggiungere - getPercentualeAttuale(veicoloElettrico); // 70% - 10% => ricarico 60%
+		double kWRicaricare = (veicoloElettrico.getCapienzaMassima() * percentualeRicaricare) / 100;
 		veicoloElettrico.setCarburanteAttuale(veicoloElettrico.getCarburanteAttuale() + kWRicaricare);
 		this.setTempoRicarica(kWRicaricare / this.getkWh()); // numero di ore di ricarica: 0,5 => 30 minuti
+		
 	}
 	
 }
