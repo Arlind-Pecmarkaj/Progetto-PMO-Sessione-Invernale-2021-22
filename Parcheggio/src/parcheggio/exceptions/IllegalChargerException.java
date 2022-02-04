38f769ec90234fde61a93b0ab0ff25d81f785f7f
petrello <tommasop01@gmail.com>
2022-02-04 18:55:07 +0100

    Create IllegalChargerException.java
    Aggiunta eccezione per gestire ricarica veicolo elettrico

---
+++ Parcheggio/src/parcheggio/exceptions/IllegalChargerException.java
@@ -0,0 +1,12 @@
+package parcheggio.exceptions;
+
+public class IllegalChargerException extends IllegalStateException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2189138802471653612L;
+	
+	public IllegalChargerException(String msg) {
+		super(msg);
+	}
+}
