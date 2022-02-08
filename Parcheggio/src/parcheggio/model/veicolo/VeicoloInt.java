b047daa9cb61dfa8c4d924a2ee9e00740fcfa4a6
MARTIN <martin.berardi15@gmail.com>
2022-02-08 17:22:11 +0100

    Create VeicoloInt.java

---
+++ Parcheggio/src/parcheggio/model/veicolo/VeicoloInt.java
@@ -0,0 +1,23 @@
+package parcheggio.model.veicolo;
+
+public interface VeicoloInt {
+	String getTarga();
+	int getAnnoImm();
+	Alimentazione getCarburante();
+	String getMarca();
+	String getModello();
+	String getNomeProprietario();
+	String getCognomeProprietario();
+	double getCapienzaMassima();
+	double getCarburanteAttuale();
+	
+	void setTarga(String t);
+	void setAnnoImm(int anno);
+	void setCarburante(Alimentazione c);
+	void setMarca(String marca);
+	void setModello(String modello);
+	void setNomeProprietario(String nome);
+	void setCognomeProprietario(String cognome);
+	void setCapienzaMassima(double capienzaMassima);
+	void setCarburanteAttuale(double carburanteAttuale);
+}
