b6793dd30fe8552436678a287c94e7ab2a6f5061
MARTIN <martin.berardi15@gmail.com>
2022-02-11 11:34:30 +0100

    Update TargheUgualiException.java

--- Parcheggio/src/parcheggio/exceptions/TargheUgualiException.java
+++ Parcheggio/src/parcheggio/exceptions/TargheUgualiException.java
@@ -1,5 +1,6 @@
 package parcheggio.exceptions;
 
+@SuppressWarnings("serial")
 public class TargheUgualiException extends RuntimeException{
 	public TargheUgualiException(String msg) {
 		super(msg);
