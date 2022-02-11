308ce5b8853e8be36e6483ad1a800091b2b5af17
MARTIN <martin.berardi15@gmail.com>
2022-02-11 11:34:39 +0100

    Update PostiFinitiException.java

--- Parcheggio/src/parcheggio/exceptions/PostiFinitiException.java
+++ Parcheggio/src/parcheggio/exceptions/PostiFinitiException.java
@@ -1,5 +1,6 @@
 package parcheggio.exceptions;
 
+@SuppressWarnings("serial")
 public class PostiFinitiException extends RuntimeException {
 	public PostiFinitiException(String msg) {
 		super(msg);
