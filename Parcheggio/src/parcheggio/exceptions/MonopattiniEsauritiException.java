ca964d3952b7697967aec54d5f5c3565512c9c85
MARTIN <martin.berardi15@gmail.com>
2022-02-11 11:34:35 +0100

    Update MonopattiniEsauritiException.java

--- Parcheggio/src/parcheggio/exceptions/MonopattiniEsauritiException.java
+++ Parcheggio/src/parcheggio/exceptions/MonopattiniEsauritiException.java
@@ -1,5 +1,6 @@
 package parcheggio.exceptions;
 
+@SuppressWarnings("serial")
 public class MonopattiniEsauritiException extends RuntimeException{
 	public MonopattiniEsauritiException(String msg) {
 		super(msg);
